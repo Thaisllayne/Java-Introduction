1b9d986ff4b2d38f5f3a0667ca54edb79ef2b319
Thaisllayne <thaisllaynesantana@gmail.com>
2021-05-06 11:43:46 -0300

    second commit

---
+++ Java/sintaxe-variaveis-e-fluxo/src/TestaConversao.java
@@ -0,0 +1,20 @@
+
+public class TestaConversao {
+
+	public static void main(String[] args) {
+		
+		// double ou float
+		double salario = 1270.50;
+		int valor = (int) salario;
+		System.out.println(valor);
+		
+		// long é para valores grandes	
+		// long numeroGrande = 23293849420;
+		
+		//short é para valores pequenos
+		//short valorPequeno = 2121;
+		
+		//byte é para valores extremamente pequenos
+		// byte b = 127;
+	}
+}
---
+++ Java/sintaxe-variaveis-e-fluxo/src/TesteVariaveis.java
@@ -0,0 +1,18 @@
+
+public class TesteVariaveis {
+	
+	public static void main(String[] args) {
+		System.out.println("ola mundo");
+		
+		// sempre declarar a variável
+		int idade;	
+		
+		idade = 37;
+		System.out.println(idade);
+		
+		idade = 30 + 10;
+		System.out.println("a idade é "+ idade);
+		
+	}
+
+}
